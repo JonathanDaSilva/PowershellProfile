54e1f760d430534286207e9c9ba78362209d4d44
JonathanDaSilva <dasilvabastos.jonathan@gmail.com>
2014-12-14 18:05:51 +0100

    Don't Update automaticly (To Slow some problem). And remove things

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,35 +1,18 @@
-Set-ExecutionPolicy Unrestricted
-# Modules
 $profileDir = Split-Path -parent $Profile
 # Test that PsGet is install
 if(-not (Test-Path $($profileDir+"\Modules\PsGet\PsGet.psm1") )) {
   (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 }
-Install-Module -ModuleURL https://github.com/JonathanDaSilva/PSTimestamp/zipball/master
-Install-Module posh-git
 
-# Update Modules
-$lastUpdate = $profileDir+'\.last-update'
-if(-not $(Test-Path $lastUpdate)) {
-  # If never update
-  Set-Content $lastUpdate 0
-}
-$time = [Int] $(Get-Content $lastUpdate)
-if( $((Get-TimeStamp) - $time) -gt $(3600*24*7)) {
-  # Check if it's hasn't been update for one week
-  Write-Host "Update Modules..." -ForegroundColor "Green"
-  Update-Module -All -Module PsGet
-  Set-Content $lastUpdate $(Get-TimeStamp)
-} else {
-  Write-Host "No Update" -ForegroundColor "Green"
-}
+Install-Module -Startup -ModuleURL https://github.com/JonathanDaSilva/PSEnvVariable/zipball/master
+Install-Module PSReadline
+# Import-Module virtualenvwrapper
 
 # Change the default Prompt
-Function Global:prompt
+function Global:prompt
 {
-  Write-Host $(Get-Location) -NoNewLine
-  Write-VcsStatus
-  return "`n>>"
+  Write-Host $(Get-Location)
+  return ">>> "
 }
 
 # Alias
@@ -52,69 +35,96 @@ function ass # AndroidScreenShot
   adb shell rm /sdcard/screenshot.png
 }
 
-function Add-Path
-{
-  param (
-    [Object]$dir = $(Get-Location)
-  )
-  if(-not (In-Path $dir)) {
-    $env:Path += ";$($dir)"
-    [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-    Write-Host "$(Get-Location) :: Added to Path" -ForegroundColor "green"
-  } else {
-    Write-Host "$(Get-Location) :: Already in Path" -ForegroundColor "red"
-  }
-}
-
-function Remove-Path
-{
-  param (
-    [Object]$dir = $(Get-Location)
-  )
-  if(In-Path) {
-    $env:Path = $env:Path.replace($dir, '')
-    $env:Path = $env:Path.replace(';;', ';')
-    [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-    Write-Host "$(Get-Location) :: Deleted for the path" -ForegroundColor "green"
-  } else {
-    Write-Host "$(Get-Location) :: Is not in the path" -ForegroundColor "red"
-  }
-}
-
-function In-Path
-{
-  param(
-    [Object]$dir = $(Get-Location)
-  )
-  $found   = $FALSE
-  foreach($path in $env:Path.split(';')) {
-    if($path -eq $dir) {
-      $found = $TRUE
-    }
-  }
-  return $found
-}
 function p
 {
   Set-Location "D:\Programmation\"
 }
 
-function qandroid
+function Remove-Service
 {
   param(
-    [String]$version = "18"
+    [Parameter(Mandatory=$True,Position=1)]
+    [String]$name = ""
+  )
+  if(Get-Service $name -ErrorAction "SilentlyContinue") {
+    $service = Get-WmiObject -Class Win32_Service -Filter "Name='$name'"
+    $service.delete()
+    Write-Host "$name Service Deleted" -ForegroundColor "Green"
+  } else {
+    Write-Host "$name Service doesn't exist" -ForegroundColor "Red"
+  }
+}
+
+function qt-cmake
+{
+  $buildDir  = "build"
+  $qtcmake   = "C:\Qt\Qt5.3.2\5.3\mingw482_32\lib\cmake\"
+  # Create the build directory if doesn't exist
+  if(-not (Test-Path $buildDir)) {
+    New-Item -ItemType Directory $buildDir
+  }
+  # build process
+  Set-Location $buildDir
+  cmake.exe -G"MinGW Makefiles" -DCMAKE_PREFIX_PATH="$qtcmake" ..
+  cmake --build .
+  Set-Location ..
+  # Copy the executable in the current folder
+  Get-ChildItem -Path $buildDir -Filter "*.exe" | foreach($_) {
+    & ".\$buildDir\$_"
+  }
+}
+
+function android-cmake
+{
+  param(
+    [String]$version = "14"
   )
   $version   = "android-" + $version
-  $directory = "build"
-  $ant       = "C:\Android\Ant\bin\ant.bat"
-  qmakeandroid
-  make
-  make install INSTALL_ROOT=build
-  deployandroid --output "$directory" --input "android-libtest.so-deployment-settings.json" --android-platform=$version --ant "$ant"
-  Copy-Item ".\$directory\bin\QtApp-debug.apk" ".\build.apk"
-
-  $pack      = (Get-Content .\build\AndroidManifest.xml | where {$_ -match 'package='}).split()[1].split('"')[1]
-  $act       = (Get-Content .\build\AndroidManifest.xml | where {$_ -match 'activity'} | where {$_ -match 'android:name'}).split('"')[1]
-  adb install -r "./build.apk"
-  adb shell am start -n $pack/$act
+  $buildDir  = "build-android"
+  $toolchain = "C:\tools\android-cmake\android.toolchain.cmake"
+  $make      = "C:\tools\android-ndk\prebuilt\windows-x86_64\bin\make.exe"
+  $qtcmake   = "C:\Qt\Qt5.3.2\5.3\android_armv7\lib\cmake\"
+  $ant       = "C:\tools\apache-ant-1.9.4\"
+  Set-Alias deploy "C:\Qt\Qt5.3.2\5.3\android_armv7\bin\androiddeployqt.exe"
+  # Create the build directory if doesn't exist
+  if(-not (Test-Path $buildDir)) {
+    New-Item -ItemType Directory $buildDir
+  }
+  # build process
+  Set-Location $buildDir
+  cmake.exe -G"MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE="$toolchain" -DCMAKE_MAKE_PROGRAM="$make" -DCMAKE_PREFIX_PATH="$qtcmake" ..
+  cmake --build .
+  # Move librarie to the current directory
+  if(Test-Path "application.so") {
+    Remove-Item "application.so"
+  }
+  if(Test-Path("..\libs\")) {
+    Copy-Item "..\libs\*" "build\libs\" -Recurse -Force
+    Remove-Item "..\libs\" -Force -Confirm:$False
+  }
+  # Create apk
+  deploy --output "build" --input "application.json" --android-platform $version
+  Copy-Item "build\bin\QtApp-Debug.apk" "application.apk"
+  Set-Location ..
 }
+
+# function qandroid
+# {
+  # param(
+  #   [String]$version = "18"
+  # )
+#   $version   = "android-" + $version
+#   $directory = "build"
+#   qmakeandroid
+#   make
+#   make install INSTALL_ROOT=build
+#   deployandroid --output "$directory" --input "android-libtest.so-deployment-settings.json" --android-platform=$version --ant "$ant"
+#   Copy-Item ".\$directory\bin\QtApp-debug.apk" ".\build.apk"
+
+#   $pack      = (Get-Content .\build\AndroidManifest.xml | where {$_ -match 'package='}).split()[1].split('"')[1]
+#   $act       = (Get-Content .\build\AndroidManifest.xml | where {$_ -match 'activity'} | where {$_ -match 'android:name'}).split('"')[1]
+#   adb install -r "./build.apk"
+#   adb shell am start -n $pack/$act
+# }
+
+Import-Module EnvVariable
